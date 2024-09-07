d3d79c78ea4539d2d84dfc7914b58af9e3f683be
Anastasia Nikitina <gunsnro@mail.ru>
2024-09-07 13:03:37 +0300

    mongo driver setup

--- blog/blog_server/server.go
+++ blog/blog_server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
@@ -8,6 +9,9 @@ import (
 	"os/signal"
 
 	"github.com/ananikitina/grpc-go/blog/blogpb"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
 )
 
@@ -15,10 +19,29 @@ type server struct {
 	blogpb.UnimplementedBlogServiceServer
 }
 
+var collection *mongo.Collection
+
+type blogItem struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty"`
+	AuthorID string             `bson:"author_id"`
+	Content  string             `bson:"content"`
+	Title    string             `bson:"title"`
+}
+
 func main() {
 	//if we crash we get the file name and lne number
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	fmt.Println("Blog service started")
+
+	fmt.Println("Connecting to MongoDB")
+	// Connect to MongoDB
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017/")
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	collection = client.Database("mydb").Collection("blog")
 
 	lis, err := net.Listen("tcp", "0.0.0.0:50051")
 	if err != nil {
@@ -41,12 +64,14 @@ func main() {
 	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, os.Interrupt)
 
-	// Block until signal is recievd
+	// Block until signal is recieved
 	<-ch
 	fmt.Println("Stopping the server")
 	s.Stop()
 	fmt.Println("Closing the listener")
 	lis.Close()
+	fmt.Println("Closing MongoDB connection")
+	client.Disconnect(context.TODO())
 	fmt.Println("End of program")
 
 }
